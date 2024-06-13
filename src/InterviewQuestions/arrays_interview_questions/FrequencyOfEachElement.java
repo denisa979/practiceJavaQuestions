b9ff430dcb58125ac70be1c4c98bb279107dfe53
denisa979 <dmutapcic79@hotmail.com>
2024-06-12 23:40:41 -0400

    FrequencyOfEachElement solution

--- src/InterviewQuestions/arrays_interview_questions/FrequencyOfEachElement.java
+++ src/InterviewQuestions/arrays_interview_questions/FrequencyOfEachElement.java
@@ -1,6 +1,47 @@
 package InterviewQuestions.arrays_interview_questions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FrequencyOfEachElement {
+    public static void main(String[] args) {
+        frequencyOfEachElement(new String[]{"Apple","Banana","Apple","Cherry"});
+
+        frequencyOfElements(new String[]{"Apple","Banana","Apple","Cherry"});
+    }
+
+    public static void frequencyOfEachElement(String[]arr){
+        Map<String, Integer> map = new HashMap<>();
+
+        //loop through each element in the array
+        for (String each : arr){
+            //increment the count of each element in the map
+            if(!map.containsKey(each)) {
+                map.put(each,0);
+            }
+            map.put(each, map.get(each)+1);
+        }
+        //print the result
+        map.forEach((k,v)-> System.out.println(k+"="+v));
+    }
+    public static void frequencyOfElements(String[]array){
+        String result ="";
+        //loop through each element in the array
+        for (String first : array){
+            int count =0;
+            //count the frequency of the current element in the array
+            for (String each : array){
+                if(first.equals(each)){
+                    count++;
+                }
+            }
+            //add the element and its frequency to the result string if not already added
+            if(!result.contains(first)){
+                result += first + "=" +count + "\n";
+            }
+        }
+        System.out.println(result);
+    }
 }
 
  /*
